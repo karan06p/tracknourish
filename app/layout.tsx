36a3c9328cb7f688ba946190149e864c0a012350
karan06p <poptanikaran1@gmail.com>
2025-06-16 19:44:26 +0530

    tweak middleware to give access to static or public assets to everyone

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,9 +38,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/public/favicon.svg" type="image/svg+xml" sizes="any" />
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,29 @@
 import { NextRequest, NextResponse } from "next/server";
 import { jwtVerify } from "jose"; // REMEMBER :- JWT tokens do not work in nextjs middleware
 
-const publicRoutes = ["/landing", "/auth/sign-in", "/auth/sign-up", "/auth/verify-email", "/auth/forgot-password", "/auth/resend-email"];
+const publicRoutes = [
+  "/landing",
+  "/auth/sign-in",
+  "/auth/sign-up",
+  "/auth/verify-email",
+  "/auth/forgot-password",
+  "/auth/resend-email",
+];
 
 const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET!);
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const accessToken = request.cookies.get("accessToken")?.value;
-  
+
+  if (
+    pathname.startsWith("/_next/") ||
+    pathname.startsWith("/static/") ||
+    pathname === "/favicon.ico" ||
+    /\.(\w+)$/.test(pathname)
+  ) {
+    return NextResponse.next();
+  }
+
   // If it's a public route
   if (publicRoutes.includes(pathname)) {
     if (accessToken) {
@@ -20,6 +36,7 @@ export async function middleware(request: NextRequest) {
     }
     return NextResponse.next();
   }
+
   // For protected routes
   if (!accessToken) {
     const refreshToken = request.cookies.get("refreshToken")?.value;
@@ -30,7 +47,7 @@ export async function middleware(request: NextRequest) {
       await jwtVerify(refreshToken, JWT_SECRET);
       return NextResponse.next();
     } catch (err) {
-      console.error("Invalid refreshToken")
+      console.error("Invalid refreshToken");
       return NextResponse.redirect(new URL("/landing", request.url));
     }
   }
@@ -44,5 +61,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next|static).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
